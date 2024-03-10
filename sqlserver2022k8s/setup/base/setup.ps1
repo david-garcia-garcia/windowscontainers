fdea0257396de603960002d36ac9a283a0da13f8
deivid.garcia.garcia@gmail.com <deivid.garcia.garcia@gmail.com>
2024-03-10 08:14:39 +0100

    xx

--- sqlserver2022k8s/setup/base/setup.ps1
+++ sqlserver2022k8s/setup/base/setup.ps1
@@ -6,7 +6,10 @@ $sqlInstance = Connect-DbaInstance -SqlInstance localhost;
 Set-DbaNetworkConfiguration -SqlInstance $sqlInstance -EnableProtocol TcpIp -Confirm:$false;
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQLServer\" -Name "LoginMode" -Value 2;
 
+# Enable SQL Server Agent
+Set-DbaSpConfigure -SqlInstance $sqlInstance -Name 'Agent XPs' -Value 1;
+
 # https://github.com/dataplat/dbatools/pull/9252
 # Install https://ola.hallengren.com/
-# Write-Host "Install https://ola.hallengren.com/";
-# Install-DbaMaintenanceSolution -SqlInstance $sqlInstance -CleanupTime 72 -LogToTable -InstallJobs;
+Write-Host "Install https://ola.hallengren.com/";
+Install-DbaMaintenanceSolution -SqlInstance $sqlInstance -CleanupTime 72 -LogToTable -InstallJobs;
