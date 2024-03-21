ba2f7ff70f92d4d653dc230d36fd968e39e4113e
deivid.garcia.garcia@gmail.com <deivid.garcia.garcia@gmail.com>
2024-03-21 08:31:51 +0100

    xx

--- sqlserver2022k8s/setup/install/setup.ps1
+++ sqlserver2022k8s/setup/install/setup.ps1
@@ -8,9 +8,31 @@ Write-Host "`n---------------------------------------"
 Write-Host " Install https://ola.hallengren.com/"
 Write-Host "-----------------------------------------`n"
 
-Install-DbaMaintenanceSolution -SqlInstance "localhost" -LogToTable;
+Install-DbaMaintenanceSolution -SqlInstance "localhost" -LogToTable -InstallJobs;
+
+# Disable jobs that we will not be using (backups are taken care of differently)
+$JobsToDisable = @(
+    "DatabaseBackup - SYSTEM_DATABASES - FULL",
+    "DatabaseBackup - USER_DATABASES - DIFF",
+    "DatabaseBackup - USER_DATABASES - FULL",
+    "DatabaseBackup - USER_DATABASES - LOG",
+    "DatabaseIntegrityCheck - SYSTEM_DATABASES",
+    "DatabaseIntegrityCheck - USER_DATABASES"
+)
+
+foreach ($JobName in $JobsToDisable) {
+    Set-DbaAgentJob -SqlInstance "localhost" -Job $JobName -Disabled;
+}
+
+# Add scheduled for the basic maintenance jobs
+
+
+Write-Host "`n---------------------------------------"
+Write-Host " Install azcopy"
+Write-Host "-----------------------------------------`n"
 
 choco install azcopy10 -y --no-progress;
 
+# Cleanup
 Get-ChildItem -Path $env:TEMP, 'C:\Windows\Temp' -Recurse | Remove-Item -Force -Recurse;
 Remove-Item -Path "$env:TEMP\*" -Recurse -Force;
