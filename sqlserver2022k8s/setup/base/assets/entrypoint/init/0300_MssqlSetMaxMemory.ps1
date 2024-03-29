434a0b69350ac4633c4c378afb8ed8c9f8bdb71a
deivid.garcia.garcia@gmail.com <deivid.garcia.garcia@gmail.com>
2024-03-29 19:50:03 +0100

    xx

--- sqlserver2022k8s/setup/base/assets/entrypoint/init/0300_MssqlSetMaxMemory.ps1
+++ sqlserver2022k8s/setup/base/assets/entrypoint/init/0300_MssqlSetMaxMemory.ps1
@@ -1,12 +1,3 @@
 # We want to set MAXMEMORY after all the startup scripts have run, so
 # that startup (and eventually restore) is not memory constrained.
-$maxMemory = SbsGetEnvInt -name "MSSQL_MAXMEMORY" -defaultValue $null;
-if (($null -ne $maxMemory)) {
-    if ($maxMemory -gt 280) {
-        SbsWriteHost "Setting max memory to $($maxMemory)Mb";
-        Set-DbaMaxMemory -SqlInstance $sqlInstance -Max $maxMemory;
-    }
-    else {
-        SbsWriteHost "Max memory is less than 280MB, not setting."
-    }
-}
+. "c:\entrypoint\refreshenv\MssqlSetMaxMemory.ps1";
