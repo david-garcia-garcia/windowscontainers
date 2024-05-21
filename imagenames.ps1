27d66d138619f95db7c32daa2f3027972d26adba
deivid.garcia.garcia@gmail.com <deivid.garcia.garcia@gmail.com>
2024-05-21 20:02:09 +0200

    XX

--- imagenames.ps1
+++ imagenames.ps1
@@ -11,12 +11,12 @@ if (-not $containerRegistry -or -not $containerRegistry.EndsWith("/")) {
 
 $version = "1.0.31";
 
-# To install MSSQL we need the ISO file, the latest CU
-$Env:MSSQLINSTALL_ISO = "https://download.microsoft.com/download/3/8/d/38de7036-2433-4207-8eae-06e247e17b25/SQLServer2022-x64-ENU-Dev.iso";
-$Env:MSSQLINSTALL_CU = "https://download.microsoft.com/download/9/6/8/96819b0c-c8fb-4b44-91b5-c97015bbda9f/SQLServer2022-KB5033663-x64.exe";
-# And a manual built patch :(, see https://github.com/microsoft/mssql-docker/issues/540
-$Env:MSSQLINSTALL_CUPATCH = "https://yourblob.blob.core.windows.net/instaladorsql/assembly_CU12.7z";
+# Installation media for MSSQL
+$Env:MSSQLINSTALL_ISO_URL = "https://download.microsoft.com/download/3/8/d/38de7036-2433-4207-8eae-06e247e17b25/SQLServer2022-x64-ENU-Dev.iso";
+$Env:MMSSQLINSTALL_CU_URL = "https://download.microsoft.com/download/9/6/8/96819b0c-c8fb-4b44-91b5-c97015bbda9f/SQLServer2022-KB5033663-x64.exe";
+$Env:MMSSQLINSTALL_CUFIX_URL = "https://yourblob.blob.core.windows.net/instaladorsql/assembly_CU12.7z";
 
+# Image names
 $Env:IMG_SERVERCORE2022 = "$($containerregistry)servercore2022:$($version)";
 $Env:IMG_SERVERCORE2022IIS = "$($containerregistry)servercore2022iis:$($version)";
 $Env:IMG_SERVERCORE2022IISNET48 = "$($containerregistry)servercore2022iisnet48:$($version)";
