20d1eff72e1f0553faacf8470642c510911efd59
deivid <deivid.garcia.garcia@gmail.com>
2024-07-15 21:20:55 +0200

    [build]

--- servercore2022iis/tests/Compose.Tests.ps1
+++ servercore2022iis/tests/Compose.Tests.ps1
@@ -5,11 +5,11 @@ Describe 'compose.yaml' {
         WaitForLog "servercore2022iis-web-1" "Initialization Completed"
     }
     It 'Responds on port 80 HTTP' {
-        (Invoke-WebRequest 172.18.8.8).RawContent | Should -Match "iisstart\.png";
+        (Invoke-WebRequest 172.18.8.8 -UseBasicParsing).RawContent | Should -Match "iisstart\.png";
     }
 
     It 'Responds on port 80 HTTP with 200 OK' {
-        (Invoke-WebRequest 172.18.8.8).StatusCode | Should -Be "200";
+        (Invoke-WebRequest 172.18.8.8 -UseBasicParsing).StatusCode | Should -Be "200";
     }
 
     AfterAll {
