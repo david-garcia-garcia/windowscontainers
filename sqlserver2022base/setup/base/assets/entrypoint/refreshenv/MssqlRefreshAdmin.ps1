a847b359ee1f91908f4b577a0d04c83f96abfd78
deivid <deivid.garcia.garcia@gmail.com>
2024-08-02 17:44:04 +0200

    xx

--- sqlserver2022base/setup/base/assets/entrypoint/refreshenv/MssqlRefreshAdmin.ps1
+++ sqlserver2022base/setup/base/assets/entrypoint/refreshenv/MssqlRefreshAdmin.ps1
@@ -6,5 +6,5 @@ $password = SbsDpapiDecode -EncodedValue $Env:MSSQL_SA_PASSWORD;
 
 if (-not([string]::IsNullOrWhiteSpace($password))) {
     $securePassword = ConvertTo-SecureString $password -AsPlainText -Force;
-    Set-DbaLogin -SqlInstance "localhost" -Login $userName -SecurePassword $securePassword -Enable -GrantLogin -PasswordPolicyEnforced:$false -Force -Confirm:$false -EnableException | Out-Null;
+    Set-DbaLogin -SqlInstance "localhost" -Login "sa" -SecurePassword $securePassword -Enable -GrantLogin -PasswordPolicyEnforced:$false -Force -Confirm:$false -EnableException | Out-Null;
 }
