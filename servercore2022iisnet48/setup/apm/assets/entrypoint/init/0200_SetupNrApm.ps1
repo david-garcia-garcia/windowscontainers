6ee835ed35e001560bfa80c36f903edec83e94a6
deivid <deivid.garcia.garcia@gmail.com>
2024-10-15 08:12:40 +0200

    XX

--- servercore2022iisnet48/setup/apm/assets/entrypoint/init/0200_SetupNrApm.ps1
+++ servercore2022iisnet48/setup/apm/assets/entrypoint/init/0200_SetupNrApm.ps1
@@ -1,18 +1,1 @@
-$envVar = [System.Environment]::GetEnvironmentVariable("NEW_RELIC_LICENSE_KEY");
-
-if (![string]::IsNullOrEmpty($envVar)) {
-    $configPath = "C:\ProgramData\New Relic\.NET Agent\newrelic.config";
-    if (Test-Path $configPath) {
-        [xml]$config = Get-Content $configPath;
-        $config.configuration.SetAttribute("agentEnabled", "true");
-        $config.configuration.service.SetAttribute("licenseKey", $envVar);
-        $config.Save($configPath);
-        SbsWriteHost "New Relic agent has been enabled.";
-    }
-    else {
-        SbsWriteHost "Config file not found at $configPath";
-    }
-}
-else {
-    SbsWriteHost "NEW_RELIC_LICENSE_KEY environment variable is not set or empty.";
-}
+. "c:\entrypoint\refreshenv\SetupNrApm.ps1";
