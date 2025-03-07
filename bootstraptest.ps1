11fbf2f005d1311be43d55a97f55066aefb4e001
deivid <deivid.garcia.garcia@gmail.com>
2025-03-07 08:42:47 +0100

    xx

--- bootstraptest.ps1
+++ bootstraptest.ps1
@@ -46,9 +46,9 @@ function WaitForLog {
     Write-Error "Timeout reached without detecting '$($logContains)' in logs after $($sw.Elapsed.TotalSeconds)s"
 }
 
-function ThrowIfError() {
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "Last exit code was NOT 0.";
+function ThrowIfError([int]$ExpectedExitCode = 0) {
+    if ($LASTEXITCODE -ne $ExpectedExitCode) {
+        Write-Error "Last exit code $($LASTEXITCODE) did not match expected code $ExpectedExitCode";
     }
 }
 
