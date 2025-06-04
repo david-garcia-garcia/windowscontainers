910df362980f8f9e5722b70cdf60aaba74c04617
deivid.garcia.garcia@gmail.com <deivid.garcia.garcia@gmail.com>
2025-06-04 12:12:09 +0200

    install winget

--- servercore2022/setup/base/setup.ps1
+++ servercore2022/setup/base/setup.ps1
@@ -18,13 +18,19 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\sppsvc" -Name "S
 Get-ScheduledTask -TaskPath "\Microsoft\Windows\SoftwareProtectionPlatform\" | Disable-ScheduledTask
 
 ################################################
+# Install winget
+################################################
+New-Item -ItemType Directory -Path "$env:TEMP\winget-cli" -Force
+Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$env:TEMP\winget-cli\winget.zip"
+Expand-Archive -LiteralPath "$env:TEMP\winget-cli\winget.zip" -DestinationPath "$env:TEMP\winget-cli" -Force
+Move-Item -Path "$env:TEMP\winget-cli\AppInstaller_x64.msix" -Destination "$env:TEMP\winget-cli\AppInstaller_x64.zip"
+Expand-Archive -LiteralPath "$env:TEMP\winget-cli\AppInstaller_x64.zip" -DestinationPath "$env:TEMP\winget-cli" -Force
+New-Item -ItemType Directory -Path "C:\winget-cli" -Force
+Move-Item -Path "$env:TEMP\winget-cli\winget.exe" -Destination "C:\winget-cli\winget.exe"
+Move-Item -Path "$env:TEMP\winget-cli\resources.pri" -Destination "C:\winget-cli\resources.pri"
+
+################################################
 # Cleanup
 ################################################
 Get-ChildItem -Path $env:TEMP, 'C:\Windows\Temp' -Recurse | Remove-Item -Force -Recurse;
 Remove-Item -Path "$env:TEMP\*" -Recurse -Force;
-
-################################################
-# Install Az.Identity, if these images are to be used in AKS,
-# we need this to deal with Workload Identity
-################################################
-Install-Module -Name Az.Identity -Force -AllowClobber -Confirm:$false;
